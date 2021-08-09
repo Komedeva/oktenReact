4b1127c87afd12d2478f0896dddaca193a232929
Huawei <katya.komedeva00@gmail.com>
2021-08-09 18:45:37 +0300

    hw1react

---
+++ src/simpsons/Simpsons.js
@@ -0,0 +1,9 @@
+export default function Simpson({name, surname, info, age, photo}) {
+    return (
+        <div className={'simpson'}>
+            <h2>{name} {surname}, age: {age}</h2>
+            <p>{info}</p>
+            <img src={photo} alt={name}/>
+        </div>
+    );
+}
