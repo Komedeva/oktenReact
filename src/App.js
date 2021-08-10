d8b8666cd636710cb37c80a3ba6a90436484b49e
Huawei <katya.komedeva00@gmail.com>
2021-08-10 14:32:29 +0300

    hw1react

--- src/App.js
+++ src/App.js
@@ -43,12 +43,7 @@ let simpsons = [
 
 function App() {
     return (
-        <div className={'simpsons'}>
-            <Simpson name={simpsons[0].name} surname={simpsons[0].surname} age={simpsons[0].age} info={simpsons[0].info} photo={simpsons[0].photo}/>
-            <Simpson name={simpsons[1].name} surname={simpsons[1].surname} age={simpsons[1].age} info={simpsons[1].info} photo={simpsons[1].photo}/>
-            <Simpson name={simpsons[2].name} surname={simpsons[2].surname} age={simpsons[2].age} info={simpsons[2].info} photo={simpsons[2].photo}/>
-            <Simpson name={simpsons[3].name} surname={simpsons[3].surname} age={simpsons[3].age} info={simpsons[3].info} photo={simpsons[3].photo}/>
-            <Simpson name={simpsons[4].name} surname={simpsons[4].surname} age={simpsons[4].age} info={simpsons[4].info} photo={simpsons[4].photo}/>
+        <div>
 
         </div>
     );
